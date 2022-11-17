7f07ef8abcd742e921fb2a61903e1d70d7fa1bc4
itayb <itay.bober@mail.huji.ac.il>
2022-11-17 20:41:03 +0200

    DRIVE TESTS

---
+++ Drive_tests/StackOverflowTest/StackTestOverflow.asm
@@ -0,0 +1,152 @@
+// C_PUSH constant 0
+@0
+D=A
+A=A+D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// C_PUSH constant 32767
+@32767
+D=A
+A=A+D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// sub
+@SP
+M=M-1
+A=M
+D=M
+A=A-1
+D=M-D
+M=D
+// C_PUSH constant 2
+@2
+D=A
+A=A+D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// C_PUSH constant 2
+@2
+D=A
+A=A+D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// C_PUSH constant 0
+@0
+D=A
+A=A+D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// C_PUSH constant 32767
+@32767
+D=A
+A=A+D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// sub
+@SP
+M=M-1
+A=M
+D=M
+A=A-1
+D=M-D
+M=D
+// C_PUSH constant 0
+@0
+D=A
+A=A+D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// C_PUSH constant 32767
+@32767
+D=A
+A=A+D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// sub
+@SP
+M=M-1
+A=M
+D=M
+A=A-1
+D=M-D
+M=D
+// C_PUSH constant 2
+@2
+D=A
+A=A+D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// C_PUSH constant 2
+@2
+D=A
+A=A+D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// C_PUSH constant 0
+@0
+D=A
+A=A+D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// C_PUSH constant 32767
+@32767
+D=A
+A=A+D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// sub
+@SP
+M=M-1
+A=M
+D=M
+A=A-1
+D=M-D
+M=D
